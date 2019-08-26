e1f28fa2f04a17676b7d6caca8baa3937f64d076
Devesh Goel <Devesh97goel@gmail.com>
2019-08-26 23:01:28 +0530

    Initial commit

--- app/src/main/java/com/tenovaters/android/writer/LoginActivity.java
+++ app/src/main/java/com/tenovaters/android/writer/LoginActivity.java
@@ -1,13 +1,18 @@
 package com.tenovaters.android.writer;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
+import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.view.KeyEvent;
 import android.view.View;
@@ -24,6 +29,13 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.tenovaters.android.writer.R;
 
 import org.w3c.dom.Text;
@@ -31,8 +43,13 @@ import org.w3c.dom.Text;
 public class LoginActivity extends AppCompatActivity {
 
 
-    private Button button;
+    private EditText emailid,passwordid;
+    private Button signin;
+    private FirebaseAuth auth;
+    ProgressDialog progressDialog;
+    private Button guest;
     private Button register;
+    private DatabaseReference databaseArtist;
 
 
     @Override
@@ -40,10 +57,22 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        button = (Button) findViewById(R.id.guest_mode);
+        emailid=(EditText)findViewById(R.id.username);
+        passwordid=(EditText)findViewById(R.id.password);
+        signin = (Button) findViewById(R.id.login);
+        guest = (Button) findViewById(R.id.guest_mode);
         register = (Button) findViewById(R.id.regiter);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        auth = FirebaseAuth.getInstance();
+
+        databaseArtist = FirebaseDatabase.getInstance().getReference("Feedback");
+        databaseArtist.child("TEst").setValue("Fevehs");
+
+        progressDialog = new ProgressDialog(LoginActivity.this);
+        progressDialog.setTitle("LOGIN");
+        progressDialog.setMessage("Logging You In");
+
+        guest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(LoginActivity.this,HomeActivity.class));
@@ -56,5 +85,76 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
             }
         });
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(haveNetwork()){
+                    String email = emailid.getText().toString().trim();
+                    String password = passwordid.getText().toString().trim();
+
+                    if (TextUtils.isEmpty(email)) {
+                        Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+                    if (TextUtils.isEmpty(password)) {
+                        Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+
+                    if (password.length() < 6) {
+                        Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+                    progressDialog.show();
+
+                    auth.signInWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    progressDialog.dismiss();
+                                    if (!task.isSuccessful()) {
+                                        // there was an error
+
+                                        Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_LONG).show();
+
+                                    } else {
+                                        Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                }
+
+                            });
+
+                }
+                else{
+                    Toast.makeText(LoginActivity.this, "Check your internet connection", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+    }
+
+    private Boolean haveNetwork() {
+        boolean have_WIFI = false;
+        boolean have_MobileData = false;
+
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo[] networkInfos = connectivityManager.getAllNetworkInfo();
+
+        for (NetworkInfo info : networkInfos) {
+            if (info.getTypeName().equalsIgnoreCase("WIFI"))
+                if (info.isConnected())
+                    have_WIFI = true;
+            if (info.getTypeName().equalsIgnoreCase("MOBILE"))
+                if (info.isConnected())
+                    have_MobileData = true;
+        }
+        return have_MobileData || have_WIFI;
+
     }
 }
--- app/src/main/java/com/tenovaters/android/writer/RegisterActivity.java
+++ app/src/main/java/com/tenovaters/android/writer/RegisterActivity.java
@@ -1,15 +1,119 @@
 package com.tenovaters.android.writer;
 
+import android.app.ProgressDialog;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText emailid,password;
+    private Button register;
+    private FirebaseAuth auth;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        emailid=(EditText)findViewById(R.id.username_reg);
+        password=(EditText)findViewById(R.id.password_reg);
+
+        register=(Button)findViewById(R.id.regiter_reg);
+
+        auth = FirebaseAuth.getInstance();
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(haveNetwork()){
+                    String email = emailid.getText().toString().trim();
+                    String passwordid = password.getText().toString().trim();
+
+
+
+                    progressDialog= new ProgressDialog(RegisterActivity.this);
+                    progressDialog.setTitle("Register");
+                    progressDialog.setMessage("Creating Your Account");
+
+                    progressDialog.setCanceledOnTouchOutside(false);
+                    progressDialog.setCancelable(false);
+
+                    if(TextUtils.isEmpty(email)){
+                        Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    }
+                    else if(TextUtils.isEmpty(passwordid)){
+                        Toast.makeText(getApplicationContext(), "Enter Your Password!", Toast.LENGTH_SHORT).show();
+                    }
+                    else if(passwordid.length()<6){
+                        Toast.makeText(getApplicationContext(), "Password Length should be greater than 6", Toast.LENGTH_SHORT).show();
+                    }
+                    else if (!isEmailValid(email)){
+                        Toast.makeText(getApplicationContext(), "Your Email Id is Invalid.", Toast.LENGTH_SHORT).show();
+
+                    }
+                    else{
+                        progressDialog.show();
+                        auth.createUserWithEmailAndPassword(email, passwordid)
+                                .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        Toast.makeText(RegisterActivity.this, "createUserWithEmail:onComplete: " + task.isSuccessful(), Toast.LENGTH_SHORT).show();
+                                        progressDialog.dismiss();
+                                        if (!task.isSuccessful()) {
+                                            Toast.makeText(getApplicationContext(), "Either there is some error or Email already exist", Toast.LENGTH_SHORT).show();
+                                        } else {
+                                            Toast.makeText(getApplicationContext(), "Account created Successfully", Toast.LENGTH_SHORT).show();
+                                            auth.signOut();
+                                        }
+                                    }
+                                });
+                    }
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "Check your internet connection", Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
+
+    }
+
+    boolean isEmailValid(CharSequence email) {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    private Boolean haveNetwork() {
+        boolean have_WIFI = false;
+        boolean have_MobileData = false;
+
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo[] networkInfos = connectivityManager.getAllNetworkInfo();
+
+        for (NetworkInfo info : networkInfos) {
+            if (info.getTypeName().equalsIgnoreCase("WIFI"))
+                if (info.isConnected())
+                    have_WIFI = true;
+            if (info.getTypeName().equalsIgnoreCase("MOBILE"))
+                if (info.isConnected())
+                    have_MobileData = true;
+        }
+        return have_MobileData || have_WIFI;
 
     }
 
